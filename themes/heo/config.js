df020405f0f5328a346c428ce96a727c76930c3c
Joanna-Beauty <827228539@qq.com>
2025-12-10 22:56:55 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,17 +18,17 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享',
   HEO_HERO_TITLE_2: '自我创造的愉悦',
-  HEO_HERO_TITLE_3: '加卷卷',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'When people accept themselves just as they are, they are more likely to change for the better',
+  HEO_HERO_TITLE_4: '嘿，来了！先看看这篇',
+  HEO_HERO_TITLE_5: '如何搭建简洁的个人知识管理系统',
+  HEO_HERO_TITLE_LINK: 'https://www.notion.so/2bc179acce4780afb360e620da89dbc2',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '随机抽一篇',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '元认知', url: '/tag/元认知' },
+  HEO_HERO_CATEGORY_2: { title: '软技能', url: '/tag/软技能' },
+  HEO_HERO_CATEGORY_3: { title: '纯干货', url: '/tag/纯干货' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -47,11 +47,11 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_URL1: '/me',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: 'https://space.bilibili.com/1469658337?spm_id_from=333.1007.0.0',
+  HEO_INFO_CARD_ICON2: 'fa-solid fa-tv',
+  HEO_INFO_CARD_URL3: 'https://github.com/Joanna-Beauty',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
